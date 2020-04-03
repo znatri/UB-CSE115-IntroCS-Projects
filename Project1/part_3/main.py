b57caa700ea57511ee91160344e893f14bfa3b66
Hardik <7909300+znatri@users.noreply.github.com>
2020-04-03 09:28:29 -0400

    Part 3 Completed

--- Project1/part_3/main.py
+++ Project1/part_3/main.py
@@ -6,6 +6,7 @@
 
 import json
 import urllib.request
+import csv
 
 def get_data(url): # url = link to json file
     response = urllib.request.urlopen(url) # Gets the HTML Code
@@ -16,7 +17,6 @@ def get_data(url): # url = link to json file
 # Testing Code for get_data()
 # url = "https://cse.buffalo.edu/~mhertz/courses/cse115/projects/mediumFile.json"
 # print(get_data(url))
-# data = get_data(url)
 
 def minimize_dictionaries(dictList, keyList):
     newList = []
@@ -31,3 +31,47 @@ def minimize_dictionaries(dictList, keyList):
 # dl = [{'tow_date': '2020-01-20', 'tow_reason': 'AB', 'state': 'NY'},
 #        {'city': 'NYC', 'tow_reason': 'ST', 'tow_date': '2015-09-20'}]
 # print(minimize_dictionaries(dl, ['tow_reason','tow_date']))
+
+def write_cache(dictList, filename):
+    # write a csv file with values of dictionaries
+    # csv file should have a header which represent the columns, each column is a key in dictionary
+    header = dictList[0].keys() # capture all the keys for header
+    recordsList = [] # stores all the records for csv file
+    for dictObj in dictList:
+        record = []
+        for val in dictObj.values():
+            record.append(val)
+        recordsList.append(record)
+    with open(filename, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(header) # write header
+        writer.writerows(recordsList) # write records from recordsList
+
+# Testing Code for write_cache()
+# url = "https://cse.buffalo.edu/~mhertz/courses/cse115/projects/mediumFile.json"
+# data = get_data(url)
+# obj = minimize_dictionaries(data, ['tow_reason','tow_date', 'state'])
+# write_cache(obj, "testingFile.csv")
+
+def read_cache(filename):
+    dictList = []
+    keys = []
+    with open(filename, 'r') as f:
+        reader = csv.reader(f)
+        header = True
+        for record in reader:
+            if header == True:
+                keys = record
+                header = False
+            else:
+                newDict = {}
+                for key, val in zip(keys, record): 
+                    newDict[key] = val 
+                # for i in range(len(keys)):
+                    # newDict[keys[i]] = record[i]
+                dictList.append(newDict)
+    return dictList
+
+# Testing Code for read_cache()
+# print(read_cache('sample.csv'))
+# print(read_cache('testingFile.csv'))
---
+++ Project1/part_4/main.py
@@ -0,0 +1,6 @@
+# CSE115LLRA Computer Science I:220110202
+# Project 1: Module 4
+# Made by Hardik Goel
+# Date Started: April 3, 2020
+# Module 4 due date: May 1, 2020
+
